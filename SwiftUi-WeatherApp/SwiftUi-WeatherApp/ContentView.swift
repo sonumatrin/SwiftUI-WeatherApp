ed2b15e0c59eb7e2bcd6477e84a54c6fa7cf37f9
Smartin5003 <Smartin5003@conestogac.on.ca>
2024-04-30 19:40:03 -0400

    Initial push

---
+++ SwiftUi-WeatherApp/SwiftUi-WeatherApp/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  SwiftUi-WeatherApp
+//
+//  Created by Sonu Martin on 2024-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .white]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+            
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight:.medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76^")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+            
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUi-WeatherApp/SwiftUi-WeatherApp/SwiftUi_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherAppApp.swift
+//  SwiftUi-WeatherApp
+//
+//  Created by Sonu Martin on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
